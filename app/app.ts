d6f0145bc81b506f129d2fbb4dbbc40c2fadef3f
ammarahm-ed <ammarahmed6506@gmail.com>
2022-10-28 23:26:05 +0500

    feat: router-view

--- app/app.ts
+++ app/app.ts
@@ -1,38 +1,16 @@
 import { createApp } from "@dominative/vue";
-//@ts-ignore
 import App from "./App.vue";
-//@ts-ignore
 import { createRouter } from "nativescript-vue-router-extended";
 import Page from "./views/Page.vue";
-//@ts-ignore
 Object.defineProperty(global, "__DEV__", { value: false });
 
 const routes = [
   {
-    path: "/app",
-    component: App,
-  },
-  {
-    path: "/page",
+    path: "/",
     component: Page,
   },
 ];
+const router = createRouter({ routes });
 const app = createApp(App);
-
+app.use(router);
 app.$run(document);
-
-const router = createRouter(
-  { routes },
-  {
-    vm: app,
-    logger: console,
-    frame: app._container,
-  }
-);
-router.setCurrentRoute({
-  component: app.component,
-  path: "/app",
-  meta: {
-    props: {},
-  },
-});
