855f63c832aa0028a16e3b3c9d3fe3353709e054
Juan de Dios Martinez Vallejo <vallemardeveloper@gmail.com>
2022-10-30 13:26:27 +0100

    refactor: addGlobalPlugin

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,4 @@
-import {createApp, addGlobalPlugin, applyPlugins} from '@dominative/vue';
+import {createApp, addGlobalPlugin} from '@dominative/vue';
 import App from './App.vue';
 import {createPinia} from "pinia";
 import {vuexStore} from "~/plugins/stores/vuex/Vuex.store";
@@ -8,11 +8,9 @@ Object.defineProperty(global, '__DEV__', {value: false});
 
 const pinia = createPinia()
 
-
-const app = createApp(App);
 addGlobalPlugin(pinia)
 addGlobalPlugin(vuexStore)
 addGlobalPlugin(router)
 
-applyPlugins(app)
+const app = createApp(App);
 app.$run();
