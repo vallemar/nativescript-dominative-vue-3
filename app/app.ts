326f99ca029686a0bcde116f570b98b08897a261
vallemar <vallemardeveloper@gmail.com>
2023-01-07 12:14:50 +0100

    updated to nativescript-community/vue

--- app/app.ts
+++ app/app.ts
@@ -1,17 +1,17 @@
-import {createApp, addGlobalPlugin} from '@dominative/vue';
-import {document} from 'dominative';
+import { createApp, addGlobalPlugin } from '@nativescript-community/vue';
+import { document } from 'dominative';
 import App from './App.vue';
-import {createPinia} from "pinia";
-import {vuexStore} from "~/plugins/stores/vuex/Vuex.store";
-import {router} from "~/plugins/router";
+import { createPinia } from 'pinia';
+import { vuexStore } from '~/plugins/stores/vuex/Vuex.store';
+import { router } from '~/plugins/router';
 
-Object.defineProperty(global, '__DEV__', {value: false});
+Object.defineProperty(global, '__DEV__', { value: false });
 
-const pinia = createPinia()
+const pinia = createPinia();
 
-addGlobalPlugin(pinia)
-addGlobalPlugin(vuexStore)
-addGlobalPlugin(router)
+addGlobalPlugin(pinia);
+addGlobalPlugin(vuexStore);
+addGlobalPlugin(router);
 
 const app = createApp(App);
-app.$run(document.createElement("ContentView"));
+app.$run(document.createElement('ContentView'));
